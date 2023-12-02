0b08e47de32b6c622bc4eea61d7738eeade98343
Fachry Azaria <fahryazaria@gmail.com>
2023-12-02 21:11:11 +0800

    Bikin Fitur Home dan How We Works

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>      
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
